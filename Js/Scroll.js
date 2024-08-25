e80b165654bc9c8f11109eebf8c4463f7d81c1a0
Gustavo Domingues <142946339+GustavoDEVVV@users.noreply.github.com>
2024-08-25 18:59:47 -0300

    SiteV0.02

---
+++ Js/Scroll.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const marqueeContent = document.getElementById('marquee-content');
+  
+    const cloneContent = marqueeContent.cloneNode(true);
+  });
+  
